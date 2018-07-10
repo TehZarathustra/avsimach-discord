2d9879a4be6f329ef463c7113c75f6e073b82d83
v-prusakov <v-prusakov@yandex-team.ru>
2018-07-10 22:09:37 +0300

    basic words

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const WORDS = {
+	buddy: /[Д-д]ружок|[Д-д]ружочек/g,
+	chack: /[Ч-ч]ак/g,
+	anime: /[А-а]ниме/g
+};
 
 bot.login(process.env.BOT_TOKEN);
 
 bot.on('message', message => {
-	if (message.content === 'дружочек') {
+	if (WORDS.buddy.test(message.content)) {
 		message.reply('пирожочек');
+	}
+
+	if (WORDS.chack.test(message.content)) {
+		message.reply('https://cdn.discordapp.com/attachments/444034088429551619/466315013607522304/5b168d61ee2cc163d01846b8.png');
+	}
+
+	if (WORDS.anime.test(message.content)) {
+		message.reply('китайские порномультики – это свинство');
 	}
 });
