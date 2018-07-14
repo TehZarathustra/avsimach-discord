304cbc9629588f8a154235f1c73c31299d08e0a2
v-prusakov <v-prusakov@yandex-team.ru>
2018-07-15 01:30:28 +0300

    tests included

---
+++ test/message-jetchart-comparison.js
@@ -0,0 +1,25 @@
+const expect = require('chai').expect;
+const {compareWeight} = require('../lib/jetchart-comparison');
+
+describe('compareWeight()', function () {
+	it('should compare weight', function () {
+		const first = {
+			name: 'MiG-15bis',
+			weight: 5044.00
+		};
+
+		const second = {
+			name: 'F-86F block 35',
+			weight: 6883.00
+		};
+
+		const expectedResults = {
+			message: 'MiG-15bis **легче**, чем F-86F block 35\n',
+			score: 0.018229579698651865
+		};
+
+		var actualResults = compareWeight(first, second);
+
+		expect(actualResults).to.deep.equal(expectedResults);
+	});
+});
